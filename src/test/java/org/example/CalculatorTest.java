676b64649e4056614eb10070ac1aeb372d8055e4
pujajathar <pujajathar91@gmail.com>
2026-08-11 14:43:48 -0500

    Practiced writing unit tests in Java

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -12,4 +12,19 @@ public class CalculatorTest {
         int result = calculator.add(2, 3);
         assertEquals(5, result, "2 + 3 should equal 5");
     }
+
+    @Test
+    public void EdgeCaseTestAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(0, 0);
+        assertEquals(0, result, "0 + 0 should equal 0");
+    }
+
+    @Test
+    public void testAddMaxValues() {
+        Calculator calculator = new Calculator();
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.add(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        });
+    }
 }
